11b935a85f7243710d88e3ef77cda43b7fd123f4
samsvk <samsvk00@gmail.com>
2022-12-13 13:42:59 +0000

    route fix

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,11 +5,11 @@ const jwt = require("jsonwebtoken");
 function routes(app) {
   app.get("/api/auth/discord/redirect", discordOAuthHandler);
   app.get("/", async (req, res) => {
-    console.log(req.cookies);
     const refresh_token = jwt.verify(req.cookies.refreshToken, process.env.PRIV_KEY);
     const access_token = jwt.verify(req.cookies.accessToken, process.env.PRIV_KEY);
-    const x = await getDiscordUser(access_token);
-    console.log(x);
+    res.status(200).json({
+      data: await getDiscordUser(access_token),
+    });
   });
 }
 
