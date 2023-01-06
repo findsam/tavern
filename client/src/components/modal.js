4b40f82bce553bb1e16df67587038b64065471e9
samsvk <samsvk00@gmail.com>
2023-01-06 00:14:08 +0000

    modal update

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { AiOutlineFileAdd } from "react-icons/ai";
 import { useRef, useEffect } from "react";
 import { sleep } from "../static/util";
 import Image from "next/image";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default ({ show, setShow }) => {
   const container = useRef(null);
@@ -31,17 +31,36 @@ export default ({ show, setShow }) => {
         ref={container}
       >
         <div className="duration-300 -translate-y-8 border rounded-lg opacity-0 border-main-border bg-main-800 main">
-          <div className="min-w-[500px] py-5">
-            <p className="px-5 text-xl">Notifications and Contributions</p>
-            <p className="px-5 text-xs tracking-wide opacity-70">
-              Set a title that people can use to search for your thread.
-            </p>
+          <div className="max-w-[550px] w-full min-w-[550px] pb-5 relative">
+            <button
+              className="absolute items-center justify-center -right-3.5 top-5 rounded-full border border-main-border bg-main-900 h-[28px] w-[28px] flex"
+              onClick={handleClose}
+            >
+              <AiOutlineClose className="text-xs opacity-70" />
+            </button>
+            <div className="flex items-center border-b border-main-border">
+              <ul className="flex h-full gap-5 px-7">
+                <li className="py-7  flex text-sm gap-1.5 after:absolute after:content-[''] after:bottom-[-1px] after:h-[2px] after:block after:bg-green-900 relative after:w-full">
+                  <span className="text-green-400">Notifications</span>
+                  <span className="h-[20px] w-[24px] border-green-900 border bg-green-900/40 text-green-400 text-center font-semibold rounded-md tracking-tighter flex items-center justify-center">
+                    3
+                  </span>
+                </li>
+                <li className="py-7  flex text-sm gap-1.5 after:absolute after:content-[''] after:block after:bg-main-border relative after:w-full">
+                  <span className=" text-white/70">Contributions</span>
+                  <span className="h-[20px] w-[24px] border-main-border  border text-white/70 rounded-md bg-main-700 text-center font-semibold  tracking-tighter flex items-center justify-center">
+                    3
+                  </span>
+                </li>
+              </ul>
+            </div>
+
             <ul className="grid">
               <NotificationItem comment={true} />
               <NotificationItem />
-              <ContributionItem />
+              {/* <ContributionItem /> */}
               <NotificationItem comment={true} />
-              <ContributionItem />
+              {/* <ContributionItem /> */}
             </ul>
           </div>
         </div>
@@ -52,7 +71,7 @@ export default ({ show, setShow }) => {
 
 const NotificationItem = (props) => {
   return (
-    <li className="flex items-start w-full gap-3 px-3 py-3 ml-auto border-b border-main-border">
+    <li className="flex items-start w-full gap-3 py-5 ml-auto border-b px-7 border-main-border">
       <Image
         className="rounded-full ring-1 ring-main-border p-0.5"
         height="38"
@@ -82,7 +101,7 @@ const NotificationItem = (props) => {
 
 const ContributionItem = () => {
   return (
-    <li className="flex flex-col items-center w-full gap-2 px-3 py-3 ml-auto border-b border-main-border">
+    <li className="flex flex-col items-center w-full gap-2 p-5 ml-auto border-b border-main-border">
       <div className="flex items-start gap-3">
         <Image
           className="rounded-full ring-1 ring-main-border p-0.5"
