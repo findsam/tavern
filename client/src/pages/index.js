f6f47969fe054ec1b8dd5bafa7613327dff2bbc4
samsvk <samsvk00@gmail.com>
2023-01-15 04:31:23 +0000

    updated size widths

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -24,10 +24,10 @@ export default function Home() {
       <Topbar user={user} />
       <Navbar user={user} setUser={setUser} />
       <div className="flex w-[calc(100%-62px)] ml-auto px-5 gap-5 pb-12 mt-[62px] pt-5">
-        <div className="fixed top-0 left-[62px] mt-[62px] pt-5 ml-5 grid gap-5 max-w-[325px] min-w-[325px] w-full">
+        {/* <div className="fixed top-0 left-[62px] mt-[62px] pt-5 ml-5 grid gap-5 max-w-[325px] min-w-[325px] w-full">
           <Community />
-        </div>
-        <div className="pl-5 ml-[325px] -z-10">
+        </div> */}
+        <div className="-z-10">
           <Feed />
         </div>
       </div>
