8ffbc2df7b881e2d090ca76d9ef658b0d2c129e1
samsvk <samsvk00@gmail.com>
2023-01-23 02:12:07 +0000

    updated notifys and auth

--- client/src/components/notifications/notificationsContainer.js
+++ client/src/components/notifications/notificationsContainer.js
@@ -1,32 +1,67 @@
 import { CommentItem } from "./commentItem";
 import { LikeItem } from "./likeItem";
 import { ContributionItem } from "./contributionItem";
+import { useState } from "react";
+
 export default () => {
+  const [activeTab, setActiveTab] = useState(1);
   return (
     <>
       <div className="relative flex items-center border-b border-main-border top-[1px] bg-main-800">
         <ul className="flex h-full gap-5 px-5">
-          <li className="py-5 flex text-sm gap-1.5 after:absolute after:content-[''] after:bottom-[-1px] after:h-[2px] after:block after:bg-green-900 relative after:w-full">
-            <span className="text-green-400">Notifications</span>
-            <span className="h-[20px] w-[24px] border-green-900 border bg-green-900/40 text-green-400 text-center font-semibold rounded-md tracking-tighter flex items-center justify-center">
+          <li
+            className={`relative py-5 flex text-sm gap-1.5 ${
+              activeTab &&
+              "after:absolute after:content-[''] after:bottom-[-1px] after:h-[2px] after:block after:bg-green-900 after:w-full"
+            }`}
+          >
+            <span className={`${activeTab ? "text-green-400" : "text-white/70"}`}>
+              Notifications
+            </span>
+            <span
+              className={`h-[20px] w-[24px] ${
+                activeTab
+                  ? "border-green-900 bg-green-900/40 text-green-400"
+                  : "border-main-border text-white/70 bg-main-700"
+              }  border text-center font-semibold rounded-md tracking-tighter flex items-center justify-center`}
+            >
               3
             </span>
           </li>
-          <li className="py-5  flex text-sm gap-1.5 after:absolute after:content-[''] after:block after:bg-main-border relative after:w-full">
-            <span className=" text-white/70">Following</span>
-            <span className="h-[20px] w-[24px] border-main-border  border text-white/70 rounded-md bg-main-700 text-center font-semibold  tracking-tighter flex items-center justify-center">
+          <li
+            className={`relative py-5 flex text-sm gap-1.5 ${
+              !activeTab &&
+              "after:absolute after:content-[''] after:bottom-[-1px] after:h-[2px] after:block after:bg-green-900 after:w-full"
+            }`}
+          >
+            <span className={`${!activeTab ? "text-green-400" : "text-white/70"}`}>
+              Following
+            </span>
+            <span
+              className={`h-[20px] w-[24px] ${
+                !activeTab
+                  ? "border-green-900 bg-green-900/40 text-green-400"
+                  : "border-main-border text-white/70 bg-main-700"
+              }  border text-center font-semibold rounded-md tracking-tighter flex items-center justify-center`}
+            >
               23
             </span>
           </li>
         </ul>
       </div>
       <ul className="grid max-h-[600px] h-full overflow-y-scroll">
-        <CommentItem />
-        <LikeItem />
-        <ContributionItem />
-        <LikeItem />
-        <CommentItem />
-        <ContributionItem />
+        {activeTab ? (
+          <>
+            <CommentItem />
+            <LikeItem />
+            <ContributionItem />
+            <LikeItem />
+            <CommentItem />
+            <ContributionItem />
+          </>
+        ) : (
+          <></>
+        )}
       </ul>
     </>
   );
--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -57,7 +57,7 @@ export default () => {
           </p>
           <a
             href={getDiscordURL()}
-            className="flex-1 px-4 min-h-[38px] gap-2 text-sm relative text-white/70 border rounded-md bg-main-700 border-main-border  hover:bg-main-900 hover:text-white duration-150 flex items-center justify-center"
+            className="flex-1 px-4 min-h-[38px] gap-2 text-sm relative text-white/70 border rounded-md bg-main-700 border-main-border  hover:bg-main-900  duration-150 flex items-center justify-center"
           >
             Sign-in with Discord
             <SiDiscord size={18} />
