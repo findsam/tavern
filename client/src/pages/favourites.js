45a5f4de16e4fe0fd3fff7200659a95f59849159
samsvk <samsvk00@gmail.com>
2023-02-05 01:35:36 +0000

    Update favourites.js

--- client/src/pages/favourites.js
+++ client/src/pages/favourites.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Context } from "../store/context";
 import Feed from "../components/feed";
 import Topbar from "../components/topbar";
+import Link from "next/link";
 import Navbar from "../components/navbar";
 
 export default () => {
@@ -11,8 +12,10 @@ export default () => {
     <>
       <Topbar />
       <Navbar />
-      <div className="flex w-[calc(100%-62px)] ml-auto px-2.5 md:px-5  pb-12 mt-[62px] pt-2.5 md:pt-5  ">
-        <Feed passedPosts={state.favourites} />
+      <div className="flex w-[calc(100%-62px)] ml-auto px-2.5 md:px-5  pb-12 mt-[62px] pt-2.5 md:pt-5">
+        {state.favourites.length > 0 && <Feed passedPosts={state.favourites} />}
+        {state.favourites.length === 0 &&
+          "You do not have any favourites at this moment in time."}
       </div>
     </>
   );
