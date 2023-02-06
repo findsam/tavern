b4eb49d3e7e299d24c1063cd1036e116557bb579
samsvk <samsvk00@gmail.com>
2023-02-06 17:09:20 +0000

    updates

--- client/src/pages/favourites.js
+++ client/src/pages/favourites.js
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import { Context } from "../store/context";
 import Feed from "../components/feed";
 import Topbar from "../components/topbar";
-import Link from "next/link";
 import Navbar from "../components/navbar";
+import Image from "next/image";
 
 export default () => {
   const { state, dispatch } = useContext(Context);
@@ -18,16 +18,22 @@ export default () => {
         </div>
       )}
       {state.favourites.length === 0 && (
-        <div className="flex flex-col gap-4 w-[calc(100%-62px)] ml-auto px-2.5 md:px-5 pb-12 mt-[62px] pt-6 md:pt-12 relative items-center justify-center">
-          <h1 className="text-2xl font-medium leading-none text-wide">
-            A story worth a thousand words...
-          </h1>
-
+        <div className="flex flex-col gap-4 w-[calc(100%-62px)] ml-auto px-2.5 md:px-5 pb-12 mt-[62px] pt-6 md:pt-12 relative items-center justify-center text-center">
+          <Image
+            quality={100}
+            loading="eager"
+            priority={true}
+            height={50}
+            width={180}
+            className="object-cover w-auto h-auto"
+            alt={"displaying duck"}
+            src={`/1.png`}
+          />
           <p className="max-w-sm text-sm leading-5 text-white/70">
-            Don't let good threads slip away! Favourite threads to find them easily
-            in the future. By favouriting threads you are directly supporting the
-            creator and contributors to continue creating the greatest stories on
-            Tavern.gg
+            We couldn't find any liked tweets... Don't let good threads slip away!
+            Favourite threads to find them easily in the future. By favouriting
+            threads you are directly supporting the creator and contributors to
+            continue creating the greatest stories on Tavern.gg
           </p>
         </div>
       )}
