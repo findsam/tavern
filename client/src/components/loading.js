88eb72a0717d2962991bb1c8dc4eda06d1077a34
samsvk <samsvk00@gmail.com>
2023-02-06 17:22:33 +0000

    skeleton loaders

--- client/src/components/loading.js
+++ client/src/components/loading.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 
 export default memo(() => {
   const randomHeight = () => Math.floor(Math.random() * (485 - 220)) + 220;
@@ -9,7 +9,7 @@ export default memo(() => {
       <div
         className="relative flex-1 w-full h-full max-w-full mx-auto my-0 overflow-hidden border rounded-lg drop-shadow-md bg-main-800 border-main-border  min-h-[100px] before:absolute before:inset-0
         before:-translate-x-full
-        before:animate-[shimmer_1s_infinite]
+        before:animate-[shimmer_2s_infinite]
         before:bg-gradient-to-r
         before:from-transparent before:via-main-border/5 before:to-transparent"
         style={{
@@ -21,7 +21,7 @@ export default memo(() => {
       <div
         className="relative flex-1 w-full h-full max-w-full mx-auto my-0 overflow-hidden border rounded-lg drop-shadow-md bg-main-800 border-main-border  min-h-[100px] before:absolute before:inset-0
         before:-translate-x-full
-        before:animate-[shimmer_1s_infinite]
+        before:animate-[shimmer_2s_infinite]
         before:bg-gradient-to-r
         before:from-transparent before:via-main-border/5 before:to-transparent"
         style={{
@@ -33,7 +33,7 @@ export default memo(() => {
       <div
         className="relative flex-1 w-full h-full max-w-full mx-auto my-0 overflow-hidden border rounded-lg drop-shadow-md bg-main-800 border-main-border  min-h-[100px] before:absolute before:inset-0
         before:-translate-x-full
-        before:animate-[shimmer_1s_infinite]
+        before:animate-[shimmer_2s_infinite]
         before:bg-gradient-to-r
         before:from-transparent before:via-main-border/5 before:to-transparent"
         style={{
