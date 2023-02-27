c1c2828c0475891e8611b90d6bc7caf2a386a0b5
samsvk <samsvk00@gmail.com>
2023-02-27 23:15:27 +0000

    updated modal working

--- client/src/components/_modal.js
+++ client/src/components/_modal.js
@@ -14,6 +14,7 @@ export default ({ show, setShow, children }) => {
     (async () => {
       if (show) {
         await sleep(1);
+        container?.current?.classList?.remove("opacity-0");
         container?.current?.childNodes[0]?.classList?.remove(
           "opacity-0",
           "translate-x-2"
@@ -24,6 +25,7 @@ export default ({ show, setShow, children }) => {
   }, [show, container]);
 
   async function handleClose() {
+    container?.current?.classList?.add("opacity-0");
     container?.current?.childNodes[0]?.classList?.add("opacity-0", "translate-x-2");
     document.body.style.overflow = "unset";
     await sleep(300);
@@ -43,10 +45,10 @@ export default ({ show, setShow, children }) => {
   return (
     show && (
       <div
-        className="fixed z-50 flex top-0 transition-all duration-75 right-0 drop-shadow-3xl h-[100%] drop-shadow-xs bg-main-900/70 w-full"
+        className="fixed z-50 flex top-0 right-0 drop-shadow-3xl h-[100%] transition-opacity ease-[cubic-bezier(.17,.67,.83,.67)] duration-75 drop-shadow-xs bg-main-800/60 w-full opacity-0"
         ref={container}
       >
-        <div className="w-full max-w-lg ml-auto duration-300 translate-x-2 border-l rounded-tl-lg rounded-bl-lg opacity-0 border-main-border bg-main-800">
+        <div className="w-full max-w-lg ml-auto [transition:transform_300ms,opacity_200ms] ease-[cubic-bezier(.17,.67,.83,.67)] translate-x-2 border-l rounded-tl-lg rounded-bl-lg opacity-0 border-main-border bg-main-800">
           <div className="relative max-h-full overflow-x-visible overflow-y-scroll">
             <button
               className="fixed z-50 flex items-center justify-center rounded-full right-5 top-5"
